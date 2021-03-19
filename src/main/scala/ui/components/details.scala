d7085e08e5f24f567e83d7c8c7080850e07a5b88
Max Gonzih <gonzih@gmail.com>
2021-03-19 10:24:00 -0400

    Meh

--- src/main/scala/ui/components/details.scala
+++ src/main/scala/ui/components/details.scala
@@ -16,10 +16,18 @@ import app.paperhands.net._
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 
+import typings.zrender.zrender.LinearGradient // not used for now, area style only expects string as color atm
 import typings.echarts.echarts._
 import typings.echarts.echarts.EChartOption._
 import typings.echarts.echarts.EChartOption.BasicComponents.CartesianAxis.DataObject
 import typings.echarts.echarts.EChartOption.BasicComponents.CartesianAxis.Type
+import typings.echarts.anon
+import typings.echarts.anon.ShadowBlur
+import typings.echarts.anon.BorderRadius
+import typings.echarts.anon.ColorOpacity
+import typings.echarts.mod.graphic
+import typings.echarts.echartsStrings
+import typings.echarts.echarts.EChartOption.BasicComponents.CartesianAxis.PointerLabel
 
 object DetailsPage {
   case class Props(
@@ -69,21 +77,64 @@ object DetailsPage {
       )
     }
 
-    def optsFromTimeseries(ts: Timeseries): EChartOption[Series] = {
+    def optsFromTimeseries(
+        legendLabel: String
+    )(ts: Timeseries): EChartOption[Series] = {
+      val as = ColorOpacity().setOpacity(0.2).setColor("rgba(128, 255, 165)")
+
+      val sb = ShadowBlur().setWidth(0)
+      val label = BorderRadius().setShow(false).setPosition("top")
       val series = SeriesLine_()
         .setType("line")
         .setData(ts.data.map(_.toDouble))
         .setSmooth(true)
+        .setLineStyle(sb)
+        .setLabel(label)
+        .setAreaStyle(as)
+        .setShowSymbol(false)
+        .setEmphasis(
+          js.Dynamic.literal("focus" -> "series").asInstanceOf[anon.`8`]
+        )
+
       val xAxis = XAxis()
         .setType(Type.category)
         .setData(ts.titles)
       val yAxis = YAxis()
         .setType(Type.value)
         .setData(js.Array())
+
+      val tb = js.Dynamic.literal(
+        "feature" -> js.Dynamic.literal(
+          "saveAsImage" -> js.Object()
+        )
+      )
+
+      val grid = Grid()
+        .setContainLabel(true)
+        .setLeft("3%")
+        .setRight("4%")
+        .setBottom("3%")
+
+      val legend = Legend_()
+        .setData(js.Array(legendLabel))
+
+      val pl = PointerLabel().setBackgroundColor("#6a7985")
+      val ap = Tooltip
+        .AxisPointer()
+        .setType(echartsStrings.line)
+        .setLabel(pl)
+      val tooltip = Tooltip_()
+        .setTrigger(echartsStrings.axis)
+        .setAxisPointer(ap)
+
       val opts = EChartOption_[Series]()
         .setXAxis(xAxis)
         .setYAxis(yAxis)
         .setSeries(js.Array(series))
+        .setToolbox(tb)
+        .setGrid(grid)
+        .setLegend(legend)
+        .setTooltip(tooltip)
 
       opts
     }
@@ -103,15 +154,15 @@ object DetailsPage {
         state.details.map(formatPopularity),
         state.details
           .map(_.engagements)
-          .map(optsFromTimeseries)
+          .map(optsFromTimeseries("Engagement"))
           .map(chrartFromOpts),
         state.details
           .map(_.mentions)
-          .map(optsFromTimeseries)
+          .map(optsFromTimeseries("Mentions"))
           .map(chrartFromOpts),
         state.details
           .map(_.sentiments)
-          .map(optsFromTimeseries)
+          .map(optsFromTimeseries("Sentiment"))
           .map(chrartFromOpts)
       )
     }
