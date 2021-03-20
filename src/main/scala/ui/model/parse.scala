8c11bb07d6761745e541b49906b0165744203ea8
Max Gonzih <gonzih@gmail.com>
2021-03-20 16:18:34 -0400

    Nicer on desktop? Maybe need separate formatting mobile/desktop?

--- src/main/scala/ui/model/parse.scala
+++ src/main/scala/ui/model/parse.scala
@@ -19,8 +19,8 @@ object Format {
     // dddd, mmmm dS, yyyy, hh:MM:ss TT
     val f = period match {
       case "1D" => "HH:MM"
-      case "5D" => "ddd"
-      case "1W" => "ddd"
+      case "5D" => "dddd"
+      case "1W" => "dddd"
       case "1M" => "mmm. d"
       case "6M" => "mmm. d, yy"
       case "1Y" => "mmm.d, yy"
